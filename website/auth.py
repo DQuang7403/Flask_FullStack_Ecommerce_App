c16284c9e72246570eda17c16f36c1d972d5deb3
Gecko403 <quangnguyen7403@gmail.com>
2023-11-07 21:49:32 +0700

    Finish front-end base

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-  return "Login"
+  return render_template('login.html')
 
-@auth.route("/sign_up")
+@auth.route("/signup")
 def sign_up():
-  return "Sign Up"
+  return render_template('signup.html')
 
 @auth.route("/logout")
 def logout():
--- website/views.py
+++ website/views.py
@@ -2,18 +2,35 @@ from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
+
 @views.route('/')
 def home():
-  return render_template('home.html')
+    return render_template('home.html')
 
-@views.route('/account')
+
+@views.route('/login')
 def account():
-  return render_template('account.html')
+    return render_template('login.html')
+
 
 @views.route('/cart')
 def shopping_cart():
-  return render_template('cart.html')
+    return render_template('cart.html')
+
 
 @views.route('/all_products')
 def all_products():
-  return render_template('all_products.html')
+    return render_template('all_products.html')
+
+@views.route('/product_detail')
+def product_detail():
+    return render_template('product_detail.html')
+
+@views.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@views.route('/contact')
+def contact():
+    return render_template('contact.html')
