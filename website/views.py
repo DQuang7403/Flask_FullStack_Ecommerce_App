66b06bdeebfb1371a207b39af410a59d39cbf4c3
Gecko403 <quangnguyen7403@gmail.com>
2023-11-22 13:33:57 +0700

    Final Product

--- website/views.py
+++ website/views.py
@@ -7,7 +7,7 @@ sqldbname = 'LAPTRINHWEB.db'
 @views.context_processor
 def my_utility_processor():
     def convert_currency_to_int(currency_str):
-        currency_str = currency_str.replace(".", "").replace(" VNĐ", "")
+        currency_str = currency_str.replace("$", "").replace(".", "")
         return int(currency_str)
     def convert_int_to_currency(number):
         number_str = str(number)
@@ -20,21 +20,26 @@ def my_utility_processor():
             currency_str = digit + currency_str
             count += 1
 
-        currency_str += " VNĐ"
+        currency_str = "$" + currency_str
         return currency_str
     return dict(convert_currency_to_int=convert_currency_to_int, convert_int_to_currency=convert_int_to_currency)
 
 @views.route('/')
 def home():
+    current_username = ""
+    if 'current_user' in session:
+        current_username = session['current_user']['name']
+    else:
+        current_username = ""
     conn = sqlite3.connect(sqldbname)
     cursor = conn.cursor()
     sqlcommand = 'SELECT * FROM FASHION LIMIT 0, 4'
     cursor.execute(sqlcommand)
     featuresProduct = cursor.fetchall()
-    sqlcommand = 'SELECT * FROM FASHION LIMIT 2, 10'
+    sqlcommand = 'SELECT * FROM FASHION LIMIT 4, 10'
     cursor.execute(sqlcommand)
     latestProducts = cursor.fetchall()
-    return render_template('home.html', featuresProduct=featuresProduct, latestProducts=latestProducts)
+    return render_template('home.html', featuresProduct=featuresProduct, latestProducts=latestProducts,user_name = current_username)
 
 @views.route('/cart', methods=['GET'])
 def shopping_cart():
@@ -51,15 +56,25 @@ def shopping_cart():
 
 @views.route('/all_products')
 def all_products():
+    current_username = ""
+    if 'current_user' in session:
+        current_username = session['current_user']['name']
+    else:
+        current_username = ""
     conn = sqlite3.connect(sqldbname)
     cursor = conn.cursor()
     sqlcommand = 'SELECT * FROM FASHION'
     cursor.execute(sqlcommand)
     all_products = cursor.fetchall()
-    return render_template('all_products.html', all_products=all_products)
+    return render_template('all_products.html', all_products=all_products,user_name = current_username)
 
 @views.route('/product_detail/<id>')
 def product_detail(id):
+    current_username = ""
+    if 'current_user' in session:
+        current_username = session['current_user']['name']
+    else:
+        current_username = ""
     conn = sqlite3.connect(sqldbname)
     cursor = conn.cursor()
     sqlcommand = 'SELECT * FROM FASHION WHERE id = ?'
@@ -68,16 +83,33 @@ def product_detail(id):
     sqlcommand = 'SELECT * FROM FASHION LIMIT 0, 4'
     cursor.execute(sqlcommand)
     related_products = cursor.fetchall()
-    return render_template('product_detail.html', product_detail=product_detail, related_products=related_products)
+    return render_template('product_detail.html', product_detail=product_detail, related_products=related_products, user_name = current_username)
 
 @views.route('/about')
 def about():
-    return render_template('about.html')
+    current_username = ""
+    if 'current_user' in session:
+        current_username = session['current_user']['name']
+    else:
+        current_username = ""
+    return render_template('about.html' ,user_name = current_username)
 
 
-@views.route('/contact')
+@views.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    current_username = ""
+    if 'current_user' in session:
+        current_username = session['current_user']['name']
+    else:
+        current_username = ""
+    if request.method == 'POST':
+        Name = request.form['Name']
+        Tel = request.form['Tel']
+        Email = request.form['Email']
+        Message = request.form['message']
+        flash("Thank you for your feedback", "success")
+        return redirect(url_for('views.contact'))
+    return render_template('contact.html',user_name = current_username)
 
 @views.route('/cart/add', methods=['POST'])
 def addToCart():
@@ -108,9 +140,8 @@ def addToCart():
     session["cart"] = cart
     rows = len(cart)
     flash('Item added successfully!', 'success')
-    # messages = ("Product added successfully! <br> Current:" +
-    #             str(rows) + " products" + "<br/><a class='btn btn-primary' href='/cart'>view cart</a><br/> <a class='btn btn-primary' href='/'>home</a>")
     return redirect(url_for('views.product_detail', id=productId))
+
 @views.route('/cart/update',methods = ['POST'])
 def CartUpdate():
     cart = session.get('cart', [])
@@ -125,36 +156,64 @@ def CartUpdate():
         new_cart.append(row)
     session['cart'] = new_cart
     return redirect(url_for('views.shopping_cart'))
+
 @views.route('/proceed_cart',methods = ['POST'] )
 def proceed_cart():
     if 'current_user' in session:
         user_id = session['current_user']['id']
         user_email = session['current_user']['email']
+        if 'cart' in session:
+            current_cart = session.get("cart",[])
+            if current_cart is not []:
+                conn = sqlite3.connect(sqldbname)
+                cur = conn.cursor()
+                status = 1
+                cur.execute('INSERT INTO "order" (user_id, user_email,status) VALUES(?, ?, ?)',(user_id,user_email,status))
+                order_id = cur.lastrowid
+                for product in current_cart:
+                    product_id = product['id']
+                    price = product['price']
+                    quantity = product['quantity']
+                    cur.execute('INSERT or REPLACE INTO "order_details" (order_id, product_id, price, quantity) VALUES(?,?,?,?)',(order_id,product_id,price,quantity))  
+                conn.commit()
+                conn.close()
+                if 'cart' in session:
+                    current_cart = session.pop("cart",[])
+                flash(f'Your order has been successfully created: orderID: {order_id}')
+                return redirect(url_for("views.account"))  
+        flash("There is nothing in cart. Let's add some!", "info")
+        return redirect(url_for("views.all_products"))
+    flash("You are not logged in. Please login", "error")
+    return redirect(url_for("auth.login"))
+    
+
+@views.route('/account/',methods = ['GET'])
+def account():
+    user_orders = []
+    current_username = ""
+    if 'current_user' in session:
+        current_username = session['current_user']['name']
     else:
-        user_id = 0
-    if 'cart' in session:
-        current_cart = session.get("cart",[])
-    conn = sqlite3.connect(sqldbname)
-    cur = conn.cursor()
-    status = 1
-    cur.execute('INSERT INTO "order" (user_id, user_email,status) VALUES(?, ?, ?)',(user_id,user_email,status))
-    order_id = cur.lastrowid
-    for product in current_cart:
-        product_id = product['id']
-        price = product['price']
-        quantity = product['quantity']
-        cur.execute('INSERT or REPLACE INTO "order_details" (order_id, product_id, price, quantity) VALUES(?,?,?,?)',(order_id,product_id,price,quantity))  
-    conn.commit()
-    conn.close()
-    if 'cart' in session:
-        current_cart = session.pop("cart",[])
-    else:
-        print("No cart in session.")
-    order_url = url_for('views.order',order_id = order_id, _external = True)
-    return f'Redirecting to order page <a href ="{order_url}">{order_url}</a>'
-@views.route('/order/',defaults = {'order_id': None},methods = ['GET'])
+        current_username = ""
+    user_id = session.get('current_user',{}).get('id')
+    if user_id:
+        conn = sqlite3.connect(sqldbname)
+        cur = conn.cursor()
+        cur.execute('Select * from "order" where user_id = ?',(user_id,))
+        user_orders = cur.fetchall()
+        conn.close()
+        return render_template('account.html',orders = user_orders, user_name = current_username)
+    flash("User not logged in")
+    return redirect(url_for("auth.login"))
+
+@views.route('/order/', defaults={'order_id':None}, methods = ['GET'])
 @views.route('/order/<int:order_id>/', methods = ['GET'])
 def order(order_id):
+    current_username = ""
+    if 'current_user' in session:
+        current_username = session['current_user']['name']
+    else:
+        current_username = ""
     user_id = session.get('current_user',{}).get('id')
     if user_id:
         conn = sqlite3.connect(sqldbname)
@@ -165,67 +224,55 @@ def order(order_id):
             cur.execute('SELECT * from order_details where order_id = ?',(order_id,))
             order_details = cur.fetchall()
             conn.close()
-            return render_template('checkout.html',order = order,order_details = order_details)
+            return render_template('orders.html',order = order,order_details = order_details, user_name = current_username)
         else:
             cur.execute('Select * from "order" where user_id = ?',(user_id,))
             user_orders = cur.fetchall()
+            if not user_orders:
+                user_orders = None
             conn.close()
-            return render_template('orders.html',orders = user_orders)
-    return "User not logged in"
-@views.route('/login', methods = ['GET','POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['txt_username']
-        password = request.form['txt_password']
-        obj_user = get_obj_user(username,password)
-        if int(obj_user[0])>0:
-            obj_user ={
-                "id" : obj_user[0],
-                "name": obj_user[1],
-                "email":obj_user[2]
-            }
-            session['current_user'] = obj_user
-        return redirect(url_for('views.shopping_cart'))
-    return render_template('login.html')
+            return render_template('account.html',orders = user_orders, user_name = current_username)
+    flash("User not logged in")
+    return redirect(url_for("auth.login"))
 
-def get_obj_user(username,password):
-    result =[]
+@views.route("/sort-form", methods=["POST"])
+def process_sort_form():
+    selected_option = request.form.get('sort-dropdown')
     conn = sqlite3.connect(sqldbname)
-    cur = conn.cursor()
-    sqlcommand = "Select * from user where email =? and password = ?"
-    cur.execute(sqlcommand,(username,password))
-    obj_user = cur.fetchone()
-    if len(obj_user)>0:
-        result = obj_user
+    cursor = conn.cursor()
+    match selected_option:
+        case "Default Sort":
+            sqlcommand = ("select * from FASHION")
+        case "Sort By Price":
+            sqlcommand = ("select * from FASHION ORDER BY CAST(REPLACE(REPLACE(price, '$', ''), '.', '') AS INT)")
+        case "Sort By Price DESC":
+            sqlcommand = ("select * from FASHION ORDER BY CAST(REPLACE(REPLACE(price, '$', ''), '.', '') AS INT) DESC")
+        case "Sort By Rating":
+            sqlcommand = ("select * from FASHION ORDER BY rating DESC")
+        case "Sort By Brand":        
+            sqlcommand = ("select * from FASHION ORDER BY brand")
+    cursor.execute(sqlcommand)
+    data = cursor.fetchall()
     conn.close()
-    return result;
-@views.route('/register', methods =['POST'])
-def register():
-    username = request.form['username']
-    email = request.form['email']
-    password = request.form['password']
-    username_error = ""
-    email_error = ""
-    password_error = ""
-    if not username: username_error = "Username is required."
-    if not password: password_error = "Password is required."
-    if not email: email_error = "Email is required."
-    if username_error or password_error or email_error:
-        return render_template("signup.html", username_error = username_error, password_error =password_error, email_error =email_error, registeration_success = "")
-    newid = SaveToDB(username, email, password)
-    stroutput = f'Registered: Username: {username},Email: {email}, Password: {password}'
-    registeration_success = "Registeration successful with id = "+ str(newid)
-    print(registeration_success+stroutput)
-    return render_template("signup.html", username_error="", password_error = "", email_error ="", registeration_success = registeration_success+stroutput)
-def SaveToDB(name,email,password):
-    conn = sqlite3.connect(sqldbname)
-    cur = conn.cursor()
-    sqlcommand = "SELECT Max(user_id) from user"
-    cur.execute(sqlcommand)
-    id_max = cur.fetchone()[0]
-    id_max = id_max + 1
-    cur.execute("INSERT INTO user (user_id, name, email, password) VALUES (?,?,?,?)",(id_max,name,email,password))
-    conn.commit()
-    conn.close()
-    return id_max
+    return render_template('all_products.html', all_products=data)
 
+@views.route('/search', methods=['POST'])
+def search():
+    current_username = ""
+    if 'current_user' in session:
+        current_username = session['current_user']['name']
+    else:
+        current_username = ""
+    search_text = request.form['searchInput']
+    if search_text == "":
+        flash("You must add some text", "error")
+    if search_text != None:
+        flash(search_text, 'success')
+        conn = sqlite3.connect(sqldbname)
+        cursor = conn.cursor()
+        sqlcommand = (
+            "select * from FASHION where product like '%") + search_text + "%'"
+        cursor.execute(sqlcommand)
+        data = cursor.fetchall()
+        conn.close()
+    return render_template('all_products.html', search_text=search_text, all_products=data, user_name = current_username)
